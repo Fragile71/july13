ff73b0fb30f103da2be5831d3cd0cfb0d88bed6e
user <user@DESKTOP-ERR5D88>
2023-07-13 17:55:25 +0900

    edit 3ways

--- july13/src/main/java/com/poseidon/web/BoardController.java
+++ july13/src/main/java/com/poseidon/web/BoardController.java
@@ -1,11 +1,11 @@
 package com.poseidon.web;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,44 +13,43 @@ import org.springframework.web.servlet.ModelAndView;
 public class BoardController {
    //사용자가 /board라고 호출하면 동작할 메소드를 만들겠습니다.
 	
+	//서비스와 연결하기
+	@Autowired 
+	private BoardService boardService; 
+	
+	
 	@GetMapping("/board")
    public ModelAndView board() {
 		ModelAndView mv = new ModelAndView("board"); //jsp파일명
-		mv.addObject("name","홍길동");
+	
+		//서비스에게 일 시킵니다.
+		List<BoardDTO> list = boardService.boardList();
+	   
+		mv.addObject("list",list);
 		
-	  List<BoardDTO> list = new ArrayList<BoardDTO>();
-	for (int i = 0; i < 10; i++) {
- 		BoardDTO e = new BoardDTO(i, i + "번째 글 입니다", "홍길동", "2023-07-" + i, i*10);
-//		e.setBno(i);
-//		e.setBtitle(i + "번째 글입니다.");
-//		e.setBwrite("홍길동");
-//		e.setBdate("2023-07-"+i);
-//		e.setBlike(i * 10);
-		list.add(e);
-	}
-	
-	
-	
-	mv.addObject("list",list);
-	
-	List<Map<String,Object>> list2 = new ArrayList<Map<String,Object>>();
-	
-	for (int i = 0; i < 10; i++) {
-		Map<String, Object> e = new HashMap<String, Object>();
-		e.put("bno", i);
-		e.put("btitle", i + "번째 글 입니다");
-		e.put("bwrite", "홍길동");
-		e.put("bdate", "2023-07-13");
-		e.put("blike", i*10);
-		list2.add(e);
-	}
-	
-	
-	mv.addObject("list2",list2);
-	
-	
+		
 		return mv;
 	}
 		
+	@GetMapping("/board2")
+	public String board2(Model model) {
+		model.addAttribute("name","홍길동");
+		
+		List<BoardDTO> list = new ArrayList<BoardDTO>();
+		for (int i = 1; i < 11; i++) {
+			BoardDTO dto = new BoardDTO(i, "제목입니다","홍길동", "2023-07-13", i);
+		list.add(dto);	
+		}
+		
+
+		model.addAttribute("list",list);
+		
+		
+		
+		
+		return "board2";
+	}
+	
+	
 	
 }
---
+++ july13/src/main/java/com/poseidon/web/board2.java
@@ -0,0 +1,5 @@
+package com.poseidon.web;
+
+public class board2 {
+
+}
